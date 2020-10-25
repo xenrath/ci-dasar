d65518681df38f716d56e2ba08750be793ee1c59
xenrath <xenrath89@gmail.com>
2020-10-25 10:38:27 +0700

    Menghilangkan index.php pada URL CodeIgniter (3)

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,8 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'buku';
+$route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
-
-$route['profile'] = 'welcome';
--- application/views/home.php
+++ application/views/home.php
@@ -1,7 +1,1 @@
 Selamat Datang
-<br><br>
-<?= $nama ?> dari <?= $alamat ?>
-<br><br>
-<?= base_url(); ?>
-<br><br>
-<?= site_url(); ?>
